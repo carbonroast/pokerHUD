8c6627fa9bed320245b115786734203da33c1e00
carbonroast <zergling3@gmail.com>
2019-07-16 22:08:15 -0400

    com

--- CaptureScreen.py
+++ CaptureScreen.py
@@ -10,10 +10,15 @@ import pywinauto
 import win32api
 import win32con
 from datetime import datetime
+import threading
 import os
+import keyboard
+from Utils import *
+from matplotlib import pyplot as plt
 
 string = ""
-
+boundary = None
+programState = None
 # Compares pixels to see if the image has changed
 def image_compare(img1, img2):
     arr1 = numpy.array(img1)
@@ -30,7 +35,7 @@ def TakeScreenshots(box):
 
             img = numpy.array(sct.grab(box))
             cv2.imshow("OpenCV/Numpy normal", img)
-            time.sleep(0.5)
+            time.sleep(0.4)
             img2 = numpy.array(sct.grab(box))
             if image_compare(img, img2):
                 print("Same Picture")
@@ -40,6 +45,23 @@ def TakeScreenshots(box):
                 cv2.imwrite(name, img2)
                 ReadScreenShot(name)
                 os.remove(name)
+            # Press q to quit
+            if cv2.waitKey(25) & 0xff == ord("q"):
+                cv2.destroyAllWindows()
+                break
+
+def TakeAndRead(box):
+    with mss.mss() as sct:
+        while "Screen capturing":
+            img = numpy.array(sct.grab(box))
+            #cv2.imshow("OpenCV/Numpy normal", img)
+            name = str(datetime.now()).replace(".","").replace(":","-") + ".png"
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            (thresh, bwimage) = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
+            cv2.imshow("OpenCV/Numpy normal", bwimage)
+            cv2.imwrite(name, bwimage)
+            ReadScreenShot(name)
+            os.remove(name)
             # Press q to quit
             if cv2.waitKey(25) & 0xff == ord("q"):
                 cv2.destroyAllWindows()
@@ -55,6 +77,8 @@ def ReadScreenShot(image):
 
 # Creates the boundaries for the bounding box
 def DrawBox():
+    global programState
+    global boundary
     while True:
         click = False
         key_code = win32con.VK_LBUTTON
@@ -73,10 +97,43 @@ def DrawBox():
             print(x2, y2)
             width = abs(x2 - x)
             height = abs(y2 - y)
+            programState = ProgramState.Screenshotting
             return {"top": min(y, y2), "left": min(x, x2), "width": width, "height": height}
+
         time.sleep(0.01)
+
 
 if __name__ == "__main__":
     print("Start")
-    boundary = DrawBox()
-    TakeScreenshots(boundary)
+
+    programState = ProgramState.Boundaries
+    while True:
+        if programState == ProgramState.Boundaries:
+            boundary = DrawBox()
+        if programState == ProgramState.Screenshotting:
+            if keyboard.is_pressed('f'):
+                TakeScreenshots(boundary)
+            if keyboard.is_pressed('t'):
+                TakeAndRead(boundary)
+
+    """
+    img_rgb = cv2.imread('playersattable.jpg')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('pokergray.jpg', img_gray)
+
+    template = cv2.imread('playerseated.png')
+    template_iogray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    h, w = template.shape[:-1]
+
+    res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+
+    threshold = 0.40
+
+    loc = numpy.where(res >= threshold)
+    print(loc)
+    for pt in zip(*loc[::-1]):
+        #print(pt, (pt[0] + w, pt[1] + h))
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+    cv2.imwrite('pokerres.png', img_rgb)
+    """
